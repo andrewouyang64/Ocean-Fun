e93a12b9cc39ce913b1c6ff176a3e0d191289c3d
andrewo <andrew.ouyang@hotmail.com>
2022-07-25 22:03:40 +1000

    jason file, auth file and home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,2 +1,25 @@
-//Dummy code line for github only
-const a= 1 
+import React from 'react';
+import kiteSurfImage from '../images/rivrt2.jpg';
+import SurfingImage from '../images/rivrt2.jpg';
+import ScubaDivingImage from '../images/rivrt2.jpg';
+
+
+export default function HomePage() {
+  return (
+    <div className='homePage'>
+     
+        <div className='kitSurf'>
+        KitSur  
+        </div>
+
+        <div className='surfing'>
+        Surfing
+        </div>
+
+        <div className='subaDiving'>
+        Suba Diving
+        </div>
+
+    </div>
+  );
+}
