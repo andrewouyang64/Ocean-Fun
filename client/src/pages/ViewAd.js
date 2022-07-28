f9ec6e11e0a5ec1c661fc4523f9acf4e5fb0cf24
andrewo <andrew.ouyang@hotmail.com>
2022-07-28 20:17:59 +1000

    viewAd page updates

--- client/src/pages/ViewAd.js
+++ client/src/pages/ViewAd.js
@@ -1,56 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
-
 import AdList from '../components/AdList';
 import CommentList from '../components/CommentList';
 import CommentForm from '../components/CommentForm';
-
 import { QUERY_ADS } from '../utils/queries';
 import { QUERY_COMMENTS } from '../utils/queries';
 import { ADD_COMMENTS } from '../utils/mutation';
 
 const ViewAd = () => {
-  const { loading1, data1 } = useQuery(QUERY_ADS);
-  const ads = data1?.ads || [];
-
-  const { loading2, data2 } = useQuery(QUERY_COMMENTS);
-  const comments = data2?.comments || [];
-
+  const { loading, data } = useQuery(QUERY_ADS);
+  const ads = data?.ads || [];
+  const [selectedAd, setSelectedAd] = useState();
   return (
-    
-      <div className="viewAd">
-        <div className='adTitle'>
-          {loading1 ? (
-            <div>Loading.....</div>
-                ) : (
-                  <AdList
+        <div className="viewAd">
+            <div className='adTitle'>
+                {loading ? (
+                <div>Loading.....</div>
+                    ) : (
+                    <AdList
                     ads={ads}
-                   />  
-               )}
+                    onSelect={(ad) => setSelectedAd(ad)}/>  
+                    )}
             </div>
     
-        <div className='adContent'>
-        {loading2 ? (
-            <div>Loading.....</div>
-                ) : (
-            <AdDescription
-            adContents={adContents} />
-              )}
+                <div className='adContent'>
+                    {selectedAd && <AdDescription adContent={selectedAd}/> }
+                </div>
         </div>
-
-
-         <div className='comments'>
-            
-            <CommentList
-            comments={comments} />
-               
-         </div>
-     
-         <div className='addComment'>
-            <CommentForm/>
-         </div>
-
-    </div>
     
   );
 };
